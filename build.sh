8ce31d98c7bfcf5992956dcf69ce221897e5d5d2
royalsoftkv <38809329+royalsoftkv@users.noreply.github.com>
2021-05-31 17:57:54 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f nclient-module-system*.tgz
-node increase_version.js
-npm pack
-cp nclient-module-system-*.tgz nclient-module-system.tgz
-curl -F "file=@./nclient-module-system.tgz" http://159.69.2.203:3030/upload
-
-
